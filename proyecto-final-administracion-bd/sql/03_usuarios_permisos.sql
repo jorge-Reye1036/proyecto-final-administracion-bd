ed31e98894df2ba4260d45f7958f126b409a502f
jorge-Reye1036 <jorgerr1032@gmail.com>
2026-08-18 15:05:14 -0600

    Update 03_usuarios_permisos.sql

--- proyecto-final-administracion-bd/sql/03_usuarios_permisos.sql
+++ proyecto-final-administracion-bd/sql/03_usuarios_permisos.sql
@@ -1,6 +1,6 @@
 -- Usuario solo lectura
 
-CREATE ROLE consulta_biblioteca LOGIN PASSWORD 'consulta123';
+CREATE ROLE consulta LOGIN PASSWORD 'consulta123';
 
 GRANT CONNECT ON DATABASE proyecto_bibliotecadb TO consulta;
 GRANT USAGE ON SCHEMA public TO consulta;
@@ -8,7 +8,7 @@ GRANT SELECT ON ALL TABLES IN SCHEMA public TO consulta;
 
 -- Usuario captura
 
-CREATE ROLE captura_biblioteca LOGIN PASSWORD 'captura123';
+CREATE ROLE captura LOGIN PASSWORD 'captura123';
 
 GRANT CONNECT ON DATABASE proyecto_bibliotecadb TO captura;
 GRANT USAGE ON SCHEMA public TO captura;
